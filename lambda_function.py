f9185d82cb2fa198767ef97f5138d6fe16be930d
vaishalijadhav15 <30460463+vaishalijadhav15@users.noreply.github.com>
2018-08-07 11:18:51 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,20 +5,19 @@ from botocore.vendored import requests
 import pandas as pd
 
 
-
-
 def download_file_to_local(url):
     # URL which needs to be download
     url = "https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.xls"
-    r = requests.get(url) # create HTTP response object
+    r = requests.get(url)  # create HTTP response object
     print("Request received")
     os.chdir('/tmp/')
-    with open("ISO_file.xlsx",'wb') as f:
-         #saving content in file(binary format)
+    with open("ISO_file.xlsx", 'wb') as f:
+        # saving content in file(binary format)
         f.write(r.content)
-    
-    
+
+
 def write_file_to_s3(file_obj, bucket_name, s3_key):
+    # initializing boto3 S3 client
     s3 = boto3.client('s3')
     s3.put_object(
         Body=file_obj,
@@ -27,36 +26,39 @@ def write_file_to_s3(file_obj, bucket_name, s3_key):
     return
 
 
-
 def lambda_handler(event, context):
     # TODO implement
-    #DOWNLOAD FILE
-    
-    context = "https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.xls"
-    #call download function
-    download_file_to_local(context)
-    print("File Downloaded")
-    
-    #PROCESS FILE
-    file = 'ISO_file.xlsx'
-    # Loading spreadsheet
-    xl = pd.ExcelFile(file)
-    
-    # Print the sheet names
-    print(xl.sheet_names)
-    
-    # Load required sheet into a DataFrame by name
-    df = xl.parse('MICs List by CC')
-    
-    # Converting each row of DF into dict with key as col name
-    my_dict = df.to_dict('record')
-    
-    #Converting list of dict into json obj
-    json_obj = json.dumps(my_dict)
-    
-    #WRITE FILE TO S3
-    #call write file to s3 function
-    write_file_to_s3(json_obj,'astro-datalake-nprod-common','test/mytest.json')
-    return 'Hello from Lambda'
+    try:
+        # DOWNLOAD FILE
 
+        file_url = "https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.xls"
+        # call download function
+        download_file_to_local(file_url)
+        print("File Downloaded")
 
+        # PROCESS FILE
+        file = 'ISO_file.xlsx'
+        # Loading spreadsheet
+        xl = pd.ExcelFile(file)
+
+        # Print the sheet names
+        print(xl.sheet_names)
+
+        # Load required sheet into a DataFrame by name
+        try:
+            df = xl.parse('MICs List by CC')
+        except:
+            print("ERROR: Sheet MICs List by CC Does not exist")
+
+        # Converting each row of DF into dict with key as col name
+        my_dict = df.to_dict('record')
+
+        # Converting list of dict into json obj
+        json_obj = json.dumps(my_dict)
+
+        # WRITE FILE TO S3
+        # call write file to s3 function
+        write_file_to_s3(json_obj, 'astro-datalake-nprod-common', 'test/mytest.json')
+        return 'Hello from Lambda'
+    except Exception as e:
+        print(e)
